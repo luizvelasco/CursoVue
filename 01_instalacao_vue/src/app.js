32d548d6199cc1a604dd7031bc5d0e8298a1d008
Luiz Velasco <luizvelasco@gmail.com>
2023-10-24 16:58:33 -0300

    inputs e data binding

--- 01_instalacao_vue/src/app.js
+++ 01_instalacao_vue/src/app.js
@@ -1,8 +1,18 @@
 const MyNameApp = {
     data(){
         return {
-            name: "João",
-            age: 30
+            name: "",
+            age: 30,
+            input_name: ""
+        }
+    },
+    methods: {
+        submitForm(e) {
+
+            e.preventDefault();
+            console.log(this.input_name);
+            this.name = this.input_name;
+
         }
     }
 }
