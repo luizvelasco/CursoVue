6b9569ceaca022fef855e02ce7e19e7b746be7e0
Luiz Velasco <luizvelasco@gmail.com>
2023-10-25 19:31:51 -0300

    data

--- 02_projeto_vue/src/App.vue
+++ 02_projeto_vue/src/App.vue
@@ -1,5 +1,8 @@
 <template>
-    <PrimeiroComponente />
+    <div>
+        <PrimeiroComponente />
+        <p>O dado de app é {{ testando }}</p>
+    </div>
 </template>
 
 <script>
@@ -9,6 +12,11 @@
         name: 'App',
         components: {
             PrimeiroComponente
+        },
+        data (){
+            return {
+                testando: "teste"
+            }
         }
     }
 </script>
--- 02_projeto_vue/src/components/PrimeiroComponente.vue
+++ 02_projeto_vue/src/components/PrimeiroComponente.vue
@@ -1,9 +1,18 @@
 <template>
-    <h1>Olá Vue</h1>
+    <div>
+        <h1>Olá Vue</h1>
+        <p>Meu nome é {{ nome }} e sou {{ profissao }}</p>
+    </div>
 </template>
 
 <script>
     export default {
-        name: 'PrimeiroComponente'
+        name: 'PrimeiroComponente',
+        data() {
+            return {
+                nome: "Velasco",
+                profissao: "Militar"
+            }
+        }
     }
 </script>
