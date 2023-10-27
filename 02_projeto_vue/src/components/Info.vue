ec6ca937e6bafb09f5d74f003dc812fcc34a7b53
Luiz Velasco <luizvelasco@gmail.com>
2023-10-27 17:58:39 -0300

    diretivas

--- 02_projeto_vue/src/components/Info.vue
+++ 02_projeto_vue/src/components/Info.vue
@@ -1,17 +1,26 @@
 <template>
     <div>
-        <p>Estou trabalhando no momento.</p>
+        <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+        <p v-else>Estou em busca de novas oportunidades</p>
         <p>Utilizo as seguintes tecologias</p>
         <ul>
             <li>PHP</li>
             <li>Javascript</li>
             <li>Python</li>
         </ul>
+        <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'Info'
+        name: 'Info',
+        data() {
+            return {
+                esta_trabalhando: false,
+                mostrar_email: true,
+                email: "luizvelasco@gmail.com"
+            }
+        }
     }
 </script>
