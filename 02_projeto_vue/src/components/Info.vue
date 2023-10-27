8fdab840be74a822b82f31cd8e5f50b6e096640e
Luiz Velasco <luizvelasco@gmail.com>
2023-10-27 19:38:48 -0300

    argumentos

--- 02_projeto_vue/src/components/Info.vue
+++ 02_projeto_vue/src/components/Info.vue
@@ -9,17 +9,24 @@
             <li>Python</li>
         </ul>
         <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
+        <p>Para acessar meu portfólio <a v-bind:href="meu_link" target="_blank">basta clicar aqui</a></p>
+        <Picture />
     </div>
 </template>
 
 <script>
+    import Picture from './Picture.vue';
     export default {
         name: 'Info',
+        components: {
+            Picture
+        },
         data() {
             return {
                 esta_trabalhando: false,
                 mostrar_email: true,
-                email: "luizvelasco@gmail.com"
+                email: 'luizvelasco@gmail.com',
+                meu_link: 'https://google.com'
             }
         }
     }
