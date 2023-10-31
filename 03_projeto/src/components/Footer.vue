7ba05853c94e551ab4ad0e689b50b6d35782c5e8
luizvelasco <luizvelasco@gmail.com>
2023-10-31 08:14:50 -0300

    footer e header

--- 03_projeto/src/components/Footer.vue
+++ 03_projeto/src/components/Footer.vue
@@ -1,5 +1,7 @@
 <template>
-    <p>Footer</p>
+    <footer id="footer">
+        <p>Make Your Burger &copy; 2023</p>
+    </footer>
 </template>
 
 <script>
@@ -7,3 +9,16 @@ export default {
     name: 'Footer'
 }
 </script>
+
+<style >
+    #footer {
+        height: 200px;
+        background-color: #222;
+        border-top: 4px solid #111;
+        color: #fcba03;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        display: flex;
+    }
+</style>
