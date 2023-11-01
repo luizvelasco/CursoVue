a4892a177ce17bd586047e942383681041254d5f
luizvelasco <luizvelasco@gmail.com>
2023-11-01 09:09:48 -0300

    dashboard

--- 03_projeto/src/views/Pedidos.vue
+++ 03_projeto/src/views/Pedidos.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="main-container">
-    <h1>PEDIDOS</h1>
+    <h1>Gerenciar Pedidos:</h1>
+    <dashboard />
   </div>
 </template>
+
+<script>
+import Dashboard from '../components/Dashboard.vue'
+export default {
+  components: { Dashboard },
+  name: "Pedidos",
+  components: {
+    Dashboard
+  }
+}
+</script>
