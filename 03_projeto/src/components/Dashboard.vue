2bb505da72a1a936a8f99171c4fa0aa1ccf9e8fd
luizvelasco <luizvelasco@gmail.com>
2023-11-01 09:50:47 -0300

    show dashboard

--- 03_projeto/src/components/Dashboard.vue
+++ 03_projeto/src/components/Dashboard.vue
@@ -11,33 +11,16 @@
                     <div>Ação:</div>
                 </div>
                 <div id="burger-table-rows">
-                    <div class="burger-table-row">
-                        <div class="order-number">1</div>
-                        <div>joão</div>
-                        <div>Pão de trigo</div>
-                        <div>maminha</div>
+                    <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+                        <div class="order-number">{{burger.id}}</div>
+                        <div>{{burger.nome}}</div>
+                        <div>{{burger.pao}}</div>
+                        <div>{{burger.carne}}</div>
                         <div>
                             <ul>
-                                <li>Salame</li>
-                                <li>Tomate</li>
-                            </ul>
-                        </div>
-                        <div>
-                            <select name="status" id="status">
-                                <option value="">Selecione</option>
-                            </select>
-                            <button class="delete-btn">Cancelar</button>
-                        </div>
-                    </div>
-                    <div class="burger-table-row">
-                        <div class="order-number">1</div>
-                        <div>joão</div>
-                        <div>Pão de trigo</div>
-                        <div>maminha</div>
-                        <div>
-                            <ul>
-                                <li>Salame</li>
-                                <li>Tomate</li>
+                                <li v-for="(opcional, index) in burger.opcionais" :key="index">
+                                    {{opcional}}
+                                </li>
                             </ul>
                         </div>
                         <div>
@@ -55,7 +38,30 @@
 
 <script>
 export default {
-  name: "Dashboard"
+  name: "Dashboard",
+  data(){
+    return {
+        burgers: null,
+        burger_id: null,
+        status: []
+    }
+  },
+  methods: {
+    async getPedidos(){
+        const req = await fetch("http://localhost:3000/burgers");
+
+        const data = await req.json();
+
+        this.burgers = data;
+
+        console.log(this.burgers);
+
+        // resgatar os status
+    }
+  },
+  mounted(){
+    this.getPedidos();
+  }
 }
 </script>
 
